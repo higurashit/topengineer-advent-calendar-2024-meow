a0a46ab742f76302a954d9de3bddba715c81e8f6
takujiru <takujiru6242@gmail.com>
2024-12-17 01:14:46 +0900

    set cat name

--- functions/findCatsGroup/lambda_function.py
+++ functions/findCatsGroup/lambda_function.py
@@ -1,6 +1,25 @@
 import random
 
 def lambda_handler(event, context):
+
+    names = [
+        'Alice', 'Bob', 'Carol', 'Charlie', 'Dave', 'Ellen', 'Eve', 'Frank', 'Isaac', 'Ivan',
+        'Justin', 'Mallory', 'Marvin', 'Mallet', 'Matilda', 'Oscar', 'Pat', 'Peggy', 'Victor',
+        'Plod', 'Steve', 'Trent', 'Trudy', 'Walter', 'Zoe', 'Darwin'
+    ]
+    my_cats = []
+    my_cats_length = random.randrange(6) + 1 # 1～7匹
+    
+    for cat in range(my_cats_length):
+        name = pick_name(names, my_cats)
+        my_cats.append(name)
+
     return {
-        "my_cats": list(range(random.randrange(6) + 1))
+        "my_cats": my_cats
     }
+
+def pick_name(names, ignores):
+    name = random.choice(names)
+    if name in ignores:
+        return pick_name(names, ignores)
+    return name
